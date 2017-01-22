2cfc45c58be85c62cdebfa0c1a143bc224285f1b
Joe Mielzarek <joseph.mielzarek@gmail.com>
2017-01-22 17:34:10 -0600

    adjusted ajax call

--- server/public/script.js
+++ server/public/script.js
@@ -1,46 +1,44 @@
+$(document).ready(function() {
+    var i = 0;
 
-$( document ).ready(function() {
-  var i = 0;
-  var gif = '';
-  var input = $('.inputBox').val("Gene");
+    function get() {
+        input = $('.inputBox').val();
+        $.ajax({
+            type: 'GET',
+            url: 'https://api.giphy.com/v1/gifs/search?q=bobs+burgers+' + input + '&api_key=dc6zaTOxFJmzC',
+            success: function(data) {
+                gif = data.data[i].images.fixed_height.url;
+                $('.container').css('background-image', 'url(' + gif + ')');
+            }
+        });
+    }
 
-  get();
+    $(".inputBox").keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            get();
+        }
+    });
 
-function get() {
-  input = $('.inputBox').val();
+    $("button.submit").click(function() {
+        get();
+    });
 
-  $.get("https://api.giphy.com/v1/gifs/search?q=bobs+burgers+'"+ input +"'&api_key=dc6zaTOxFJmzC", function(data, status){
-    gif = data.data[i].images.fixed_height.url;
-  });
+    $("button.next").click(function() {
+        i++;
+        get();
+    });
 
-  $('.container').css('background-image', 'url(' + gif + ')');
-  console.log(gif);
-}
+    $("button.url").click(function() {
+        alert(gif);
+    });
 
+    $('.controls').mouseenter(function() {
+        $('.container').addClass('blur');
+    });
 
-  $("button.submit").click(function(){
-    get();
-  });
-
-  $("button.next").click(function(){
-    i++;
-    get();
-  });
-
-  $("button.url").click(function(){
-    alert(gif);
-  });
-
-
-  $('.controls').mouseenter( function() {
-    $('.container').addClass('blur');
-  });
-
-  $('.controls').mouseleave( function() {
-    $('.container').removeClass('blur');
-  });
-
-
-
+    $('.controls').mouseleave(function() {
+        $('.container').removeClass('blur');
+    });
 
 });
